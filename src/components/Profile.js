b178cb29ac1b51fa1bf6df73285f0a50f22091fe
Your Name <you@example.com>
2021-08-04 11:58:12 +0300

    Урок 13

---
+++ src/components/Profile.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+        <img src="https://i.ytimg.com/vi/zmX24E_kyhM/maxresdefault.jpg" alt=""/>
+        </div>
+        <div>
+        ava + description
+        </div> 
+        <div>
+        my posts
+            <div>
+            new post
+            </div>
+            <div>
+            post 1
+            </div>
+            <div>
+            post 2
+            </div>
+        </div> 
+    </div>
+}
+
+export default Profile;
